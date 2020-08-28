f308da0a4e21427674d4dfdc4bcb6210eb86cb56
Sujith Patil <sujithpatil5@gmail.com>
2020-08-28 11:15:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+const express = require('express');
+const { Server } = require('ws');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
 //require our websocket library 
 var WebSocketServer = require('ws').Server;
 
 //creating a websocket server at port 9090 
-var wss = new (require('ws')).Server({port: (process.env.PORT || 5000)});
+var wss = new Server({ server });
 
 //all connected to the server users 
 var users = {};
